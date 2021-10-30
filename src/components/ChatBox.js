2967b5eb37f3578f229852c0b17b22bdf1058016
nastaranjalali <nastaranjalalibusiness@gmail.com>
2021-10-30 12:31:26 +0330

    fit: add fuctinality to send button and scroller for messages

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -3,19 +3,41 @@ import AdminMessage from "./AdminMessage";
 import "./ChatBox.styles.css";
 import UserMessage from "./UserMessage";
 import SendIcon from "../assets/icons/sendIcon.png";
+import { useState } from "react";
 
 export default function ChatBox({ isComponentVisible }) {
+  const [chatList, setChatList] = useState([]);
+  var [InputMessage, setInputMessage] = useState("");
+
+  const sendMessage = () => {
+    setChatList(
+      chatList.concat(
+        <div key={chatList.length}>
+          <UserMessage message={InputMessage} />
+          <AdminMessage message="hi this is admin how can I help you?" />
+        </div>
+      )
+    );
+  };
+
   return (
     isComponentVisible && (
       <div onBlur={(e) => e.stopPropagation} className="containerChat">
         <div className="message">
-          <AdminMessage message="hi this is admin" />
-          <UserMessage message="hi this is user" />
+          <AdminMessage message="hi this is admin how can I help you?" />
+          {chatList}
+          {}
         </div>
         <div className="inputContainer">
-          <input type="text" className="chatInput" />
-          <button type="submit" className="sendButton">
-            <img src={SendIcon} className="sendIcon" />
+          <input
+            required
+            type="text"
+            className="chatInput"
+            value={InputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
+          />
+          <button type="submit" className="sendButton" onClick={sendMessage}>
+            <img src={SendIcon} className="sendIcon" alt="ss" />
           </button>
         </div>
       </div>
