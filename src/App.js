e3cbc099478822f471336f749cc44e1e6e33a7e9
nastaranjalali <nastaranjalalibusiness@gmail.com>
2021-10-31 11:42:02 +0330

    fix: some styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import ChatWidget from './components/ChatWidget';
+import "./App.css";
+import ChatWidget from "./components/ChatWidget";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <ChatWidget/>
+      <ChatWidget />
     </div>
   );
 }
